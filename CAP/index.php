a799c85b46e418a8b90c3f1d4639f99d374ae8f5
Primasdika Yunia Putra <dickayunia1@gmail.com>
2019-10-21 07:51:53 +0700

    Menambahkan index.php CAP dan edit Prototype

---
+++ CAP/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cahaya Abadi Perkasa</title>
+</head>
+<body>
+    <header>
+        <div id="logo">
+            <a href="index.php">
+                <h1>Cahaya Abadi Perkasa</h1>
+                <h2>Gesit dan Professional</h2>
+            </a>
+        </div>
+
+        <nav>
+            <ul class="akunuser">
+                <li><a href="#">Produk</a></li>
+                <li><a href="#">Testimoni</a></li>
+                <li><a href="#">Cara Kerja</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="container">
+        <section class="slide">
+            
+        </section>
+
+        <section id="carakerja">
+
+        </section>
+
+        <section id="produk">
+
+        </section>
+
+        <section id="testimoni">
+            
+        </section>
+
+        <footer>
+            <div id="contactus">
+
+            </div>
+            <div class="copyright">
+
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
