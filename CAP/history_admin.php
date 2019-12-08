de7d365184e91d677b6a1b34490ee515c099bfb3
dytashofia <dytashofia32@gmail.com>
2019-12-08 08:14:34 +0700

    history, footer, dan js

--- CAP/history_admin.php
+++ CAP/history_admin.php
@@ -1,75 +1,105 @@
 <?php 
+
+require 'includes/config.php';
 require 'includes/header.php';
+
+$_SESSION['id_admin'] = 'ADM000002';
+$id_admin = $_SESSION['id_admin'];
+$result_pesanan = mysqli_query($con, "SELECT * FROM pesanan, user where
+user.USER_ID = pesanan.USER_ID AND 
+ADM_ID='$id_admin'")
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://www.google.com/search?q=bootstrap+4.3&oq=BOOT&aqs=chrome.0.69i59j69i57j69i59l2j35i39j69i60l3.3349j0j4&sourceid=chrome&ie=UTF-8">
-    <title>CSS</title>
-</head>
 
 <!-- History User -->
-<body>
-  <div class="history">
-    <h1>HISTORY</h1>
+<div class="container">
+  <div class="card shadow m-5">
+    <div class="card-header text-center">
+      <h2 clas="">History Admin</h2>
+    </div>
+    <div class="card-body">
+    <table id="example" class="table table-bordered" style="width:100%">
+        <thead>
+              <tr>
+                  <th>No</th>
+                  <th>ID Pesanan</th>
+                  <th>User</th>
+                  <th>Tanggal</th>
+                  <th>Total</th>
+                  <th>Status</th>
+                  <th>Ket</th>
+              </tr>
+        </thead>
+        <?php 
+        $i=0;
+        while($data_pesanan = mysqli_fetch_assoc($result_pesanan)){
+            $i+=1;
+            $id_psn = $data_pesanan['ID_PESANAN'];
+            $total_psn = $data_pesanan['TOTAL_HARGA'];
+            $nama_user = $data_pesanan['USER_NAMA_LENGKAP'];
+            $tanggal_psn = $data_pesanan['TANGGAL_PESANAN'];
+            $status_psn = $data_pesanan['STATUS_PESANAN'];
+            $ket_pembayaran = $data_pesanan['KET_PEMBAYARAN'];
+        ?>
+          
+          <tbody>
+              <tr class="bg-light">
+                  <td><?=$i?></td>
+                  <td><?=$id_psn?></td>
+                  <td><?=$nama_user?></td>
+                  <td><?=$tanggal_psn?></td>
+                  <td><?=$total_psn?></td>
+                  <td><?=$status_psn?></td>
+                  <td><?=$ket_pembayaran?></td>
+              </tr>
+          </tbody>
+          <thead>
+              <tr class="table-primary">
+                    <th></th>
+                  <th>Nama Prd</th>
+                  <th>Warna</th>
+                  <th>Ukuran</th>
+                  <th>Jumlah Prd</th>
+                  <th>Ket Desain</th>
+                  <th>Sub Total</th>
+              </tr>
+          </thead>
+        <?php
+        $result_produk = mysqli_query($con, "SELECT produk.NAMA_PRODUK, warna.JENIS_WARNA, ukuran.JENIS_UKURAN, detail_pesanan.JUMLAH_PRODUK, detail_pesanan.SUB_TOTAL, detail_pesanan.STATUS_DESAIN FROM
+        produk, detail_pesanan, warna, ukuran
+        WHERE
+        produk.ID_PRODUK = detail_pesanan.ID_PRODUK AND
+        warna.ID_WARNA = produk.ID_WARNA AND
+        ukuran.ID_UKURAN = produk.ID_UKURAN AND
+        detail_pesanan.ID_PESANAN = '$id_psn'");
+        while($data_produk = mysqli_fetch_assoc($result_produk)){
+        $nama_produk = $data_produk['NAMA_PRODUK'];
+        $warna_produk = $data_produk['JENIS_WARNA'];
+        $ukuran_produk = $data_produk['JENIS_UKURAN'];
+        $jml_produk = $data_produk['JUMLAH_PRODUK'];
+        $sub_total = $data_produk['SUB_TOTAL'];
+        $ket_desain = $data_produk['STATUS_DESAIN'];
+        ?>
+        
+          <tbody>
+              <tr class="table-warning">
+                    <th></th>
+                  <td><?=$nama_produk?></td>
+                  <td><?=$warna_produk?></td>
+                  <td><?=$ukuran_produk?></td>
+                  <td><?=$jml_produk?></td>
+                  <td><?=$sub_total?></td>
+                  <td><?=$ket_desain?></td>
+              </tr>
+          </tbody>
+        <?php } }?>
+      </table>
+    </div>
   </div>
-<br>
-
-<!-- Untuk Tanggal -->
-<!-- Label Kalender -->
-  <div class="form-group col-md-4">
-      <label for="inputState">Tanggal</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-      </select>
-  </div>
-
-<!-- Untuk Tabel -->
-  <table class="table">
-    <thead class="thead-dark">
-      <tr>
-        <th scope="col">ID Pesanan</th>
-        <th scope="col">ID User</th>
-        <th scope="col">Nama Pemesan</th>
-        <th scope="col">Tanggal Pesanan</th>
-        <th scope="col">Pesanan</th>
-        <th scope="col">Total Harga</th>
-        <th scope="col">Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
-      </tr>
-    </tbody>
-  </table>
-<br>
-<br>
-</body>
+</div>
+  
 
 <?php
-require 'includes/footer.php';
+require 'includes/footer.php'
 ?>
 </html>
--- CAP/src/js/script.js
+++ CAP/src/js/script.js
@@ -1,90 +1,90 @@
 /* SCRIPT UNTUK MENGHILANGKAN ALERT SECARA OTOMATIS SELAMA 2 DETIK */
 
-$("#alert-login").fadeTo(2000, 500).slideUp(500, function(){
+$("#alert-login").fadeTo(2000, 500).slideUp(500, function () {
   $("#alert-login").slideUp(500);
   history.pushState(null, null, window.location.href.split('#')[0]);
   window.location.hash = '';
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   $('#example').DataTable();
-} );
+});
 
 /* SCRIPT UNUTUK MENAMPILKAN KATA SANDI KETIKA DICENTANG */
 
-$(document).ready(function(){		
-  $('#tampil-sandi').click(function(){
-    if($(this).is(':checked')){
-      $('.tampil-sandi').attr('type','text');
-    }else{
-      $('.tampil-sandi').attr('type','password');
+$(document).ready(function () {
+  $('#tampil-sandi').click(function () {
+    if ($(this).is(':checked')) {
+      $('.tampil-sandi').attr('type', 'text');
+    } else {
+      $('.tampil-sandi').attr('type', 'password');
     }
   });
 
-  $("#select_bahan").change(function(){
-    $(this).find("option:selected").each(function(){
-        var optionValue = $(this).attr("value");
-        if(optionValue){
-            $(".box_bahan").not("#" + optionValue).hide();
-            $("#" + optionValue).show();
-        } else{
-            $(".box_bahan").hide();
-        }
+  $("#select_bahan").change(function () {
+    $(this).find("option:selected").each(function () {
+      var optionValue = $(this).attr("value");
+      if (optionValue) {
+        $(".box_bahan").not("#" + optionValue).hide();
+        $("#" + optionValue).show();
+      } else {
+        $(".box_bahan").hide();
+      }
     });
   }).change();
 
-  $("#select_ukuran").change(function(){
-    $(this).find("option:selected").each(function(){
-        var optionValue = $(this).attr("value");
-        if(optionValue){
-            $(".box_ukuran").not("#" + optionValue).hide();
-            $("#" + optionValue).show();
-        } else{
-            $(".box_ukuran").hide();
-        }
+  $("#select_ukuran").change(function () {
+    $(this).find("option:selected").each(function () {
+      var optionValue = $(this).attr("value");
+      if (optionValue) {
+        $(".box_ukuran").not("#" + optionValue).hide();
+        $("#" + optionValue).show();
+      } else {
+        $(".box_ukuran").hide();
+      }
     });
   }).change();
 });
 
 // MENAMPILAKN UPLOAD GAMBAR SAAT DI PILIH
 
-$(document).ready(function() {
+$(document).ready(function () {
   // Kondisi saat Form di-load
-  if($('input[id="pilihdesain1"]:radio:checked').val()=="Y"){
+  if ($('input[id="pilihdesain1"]:radio:checked').val() == "Y") {
+    $('#uploadfile').removeAttr('disabled');
+  } else {
+    $('#uploadfile').attr('disabled', 'disabled');
+  }
+  // Kondisi saat Radio Button diklik
+  // $('input[type="radio"]').click(function(){
+  $('input[id="pilihdesain1"]:radio').click(function () {
+    if ($(this).attr("value") == "N") {
+      $('#uploadfile').attr('disabled', 'disabled');
+    } else {
       $('#uploadfile').removeAttr('disabled');
-  } else {
-      $('#uploadfile').attr('disabled','disabled'); 
-  }
-  // Kondisi saat Radio Button diklik
-  // $('input[type="radio"]').click(function(){
-  $('input[id="pilihdesain1"]:radio').click(function(){
-      if($(this).attr("value")=="N"){
-          $('#uploadfile').attr('disabled','disabled'); 
-      } else {
-          $('#uploadfile').removeAttr('disabled');
-          $('#uploadfile').focus();
-      } 
+      $('#uploadfile').focus();
+    }
   });
-}); 
+});
 
-$(document).ready(function() {
+$(document).ready(function () {
   // Kondisi saat Form di-load
-  if($('input[id="pilihdesain2"]:radio:checked').val()=="Y"){
-      $('#uploadfile').attr('disabled','disabled'); 
+  if ($('input[id="pilihdesain2"]:radio:checked').val() == "Y") {
+    $('#uploadfile').attr('disabled', 'disabled');
   } else {
-      $('#uploadfile').attr('disabled','disabled'); 
+    $('#uploadfile').attr('disabled', 'disabled');
   }
   // Kondisi saat Radio Button diklik
   // $('input[type="radio"]').click(function(){
-  $('input[id="pilihdesain2"]:radio').click(function(){
-      if($(this).attr("value")=="N"){
-          $('#uploadfile').attr('disabled','disabled'); 
-      } else {
-          $('#uploadfile').attr('disabled','disabled');
-          $('#uploadfile').focus();
-      } 
+  $('input[id="pilihdesain2"]:radio').click(function () {
+    if ($(this).attr("value") == "N") {
+      $('#uploadfile').attr('disabled', 'disabled');
+    } else {
+      $('#uploadfile').attr('disabled', 'disabled');
+      $('#uploadfile').focus();
+    }
   });
-}); 
+});
 
 $("#select_warna").change(function () {
   $(this).find("input:checked").each(function () {
@@ -113,6 +113,7 @@ $(document).on('click', 'body *', function () {
   var ValDesain = 0;
   var HrgDesain = 50000;
   var ValPembayaran = 0;
+  var JmlCetak = 0;
   var JmlCetak = document.getElementById('jumlah_produk').value;
 
   $("#select_warna").change(function () {
@@ -158,7 +159,7 @@ $(document).on('click', 'body *', function () {
   console.log(dataukuran);
 
   $("#keranjang").attr("data-ukuran", HrgUkuran);
-  
+
 
 });
 
@@ -167,22 +168,22 @@ function validate(evt) {
 
   // Handle paste
   if (theEvent.type === 'paste') {
-      key = event.clipboardData.getData('text/plain');
+    key = event.clipboardData.getData('text/plain');
   } else {
-  // Handle key press
-      var key = theEvent.keyCode || theEvent.which;
-      key = String.fromCharCode(key);
+    // Handle key press
+    var key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode(key);
   }
   var regex = /[0-9]|\./;
-  if( !regex.test(key) ) {
+  if (!regex.test(key)) {
     theEvent.returnValue = false;
-    if(theEvent.preventDefault) theEvent.preventDefault();
+    if (theEvent.preventDefault) theEvent.preventDefault();
   }
 }
 
 // Upload gambar
 
-$('.custom-file-input').on('change',function(){
+$('.custom-file-input').on('change', function () {
   let fileName = $(this).val().split('\\').pop();
   $(this).next('.custom-file-label').addClass("custom-file").html(fileName);
 });
@@ -191,5 +192,5 @@ $('.custom-file-input').on('change',function(){
 
 $('#ceksemua').click(function () {
   $(this).parents('fieldset:eq(0)').find(':checkbox').attr('checked', this.checked);
-  });
+});
 
